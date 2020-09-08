7c34a7dde5a537a517b1b863c7c0fb0b7503f8e8
Josef Erben <josef@erben.io>
2020-09-08 08:27:05 +0200

    Update to use Sihl 0.1.1

--- service.ml
+++ service.ml
@@ -1,15 +1,20 @@
 (* Kernel services *)
-module Random = Sihl.Utils.Random.Service
-module Log = Sihl.Log.Service
+module Random = Sihl.Utils.Random.Service.Make ()
+
+module Log = Sihl.Log.Service.Make ()
+
 module Config = Sihl.Config.Service.Make (Log)
 module Db = Sihl.Data.Db.Service.Make (Config) (Log)
 module MigrationRepo = Sihl.Data.Migration.Service.Repo.MakeMariaDb (Db)
-module Cmd = Sihl.Cmd.Service
+
+module Cmd = Sihl.Cmd.Service.Make ()
+
 module Migration =
   Sihl.Data.Migration.Service.Make (Log) (Cmd) (Db) (MigrationRepo)
-module WebServer = Sihl.Web.Server.Service.Make (Cmd)
+module WebServer = Sihl.Web.Server.Service.MakeOpium (Log) (Cmd)
 module Schedule = Sihl.Schedule.Service.Make (Log)
-module Repo = Sihl.Data.Repo.Service
+
+module Repo = Sihl.Data.Repo.Service.Make ()
 
 (* Configuration providers *)
 module EmailConfigProvider = Sihl.Email.Service.EnvConfigProvider (Config)
@@ -22,4 +27,4 @@ module EmailTemplateRepo =
 module EmailTemplate =
   Sihl.Email.Service.Template.Make (Log) (EmailTemplateRepo)
 module Email =
-  Sihl.Email.Service.Make.Smtp (EmailTemplate) (EmailConfigProvider)
+  Sihl.Email.Service.Make.Smtp (Log) (EmailTemplate) (EmailConfigProvider)
